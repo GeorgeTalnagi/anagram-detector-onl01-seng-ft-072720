b87f247355c6f016ecd223545eb1e5624c717443
George Talnagi <gtalnagi@yahoo.com>
2020-08-07 22:24:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+attr_accessor :name
+
+def initialize(name)
+  @name = name 
+  end
+
+def match(name) 
+  name.collect do |words|
+      @name 
+    end 
+  end 
+
+
+end 
