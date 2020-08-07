f8b070e41650367db4553bca0f80f479ac8ab598
George Talnagi <gtalnagi@yahoo.com>
2020-08-07 22:53:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ attr_accessor :word
     anagrams = []
     binding.pry
     words.each do |new_word|
-      if @word.split().sort == new_word.split().sort 
+      if @word.split("").sort == new_word.split().sort 
         anagrams << new_word 
       end
     end 
